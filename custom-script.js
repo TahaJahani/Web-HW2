71c6b98aeaa2b6f4f2ea5a191cb4968b1559efc9
TahaJahani <taha7900@gmail.com>
2021-12-02 20:37:42 +0330

    toast added

--- custom-script.js
+++ custom-script.js
@@ -15,3 +15,14 @@ function randomGradient() {
     let randomColor2 = 'rgba(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + 0.7 + ')';
     gradient.style.background = "linear-gradient(90deg, " + randomColor1 + " 0%, " + randomColor2 + " 100%)";
 }
+
+function showToast() {
+  // Get the snackbar DIV
+  var x = document.getElementById("toast");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
