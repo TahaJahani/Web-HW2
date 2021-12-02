bd24599326634024765c6a85e705304e0b75d607
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-12-02 13:23:17 +0330

    Change gradient randomly on button click

--- custom-script.js
+++ custom-script.js
@@ -2,3 +2,11 @@ function toggleDarkMode() {
   let element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function randomGradient() {
+    let gradient = document.querySelector("#mask");
+
+    let randomColor1 = 'rgba(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + 1 + ')';
+    let randomColor2 = 'rgba(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + 0.7 + ')';
+    gradient.style.background = "linear-gradient(90deg, " + randomColor1 + " 0%, " + randomColor2 + " 100%)";
+}
