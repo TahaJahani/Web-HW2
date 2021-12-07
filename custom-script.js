b99f4ca30bd91149d65ea6e196a684494309457f
TahaJahani <taha7900@gmail.com>
2021-12-07 11:55:36 +0330

    mobile side navbar added

--- custom-script.js
+++ custom-script.js
@@ -38,3 +38,15 @@ function contactUsMouseMethod(e) {
     yellowText.style.fontSize = fontSize;
   }
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("overlay").style.display = "block";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("overlay").style.display = "none";
+}
